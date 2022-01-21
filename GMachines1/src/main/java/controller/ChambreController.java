7faa001a90c504a8ce40a803d6045401a06e8192
mehdi6ajana <98132838+mehdi6ajana@users.noreply.github.com>
2022-01-21 23:41:05 +0100

    Added pom

--- GMachines1/src/main/java/controller/ChambreController.java
+++ GMachines1/src/main/java/controller/ChambreController.java
@@ -12,10 +12,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import org.apache.commons.io.FilenameUtils;
 
 import beans.Chambre;
 import beans.User;
